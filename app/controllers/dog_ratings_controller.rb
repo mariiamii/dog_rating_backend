38807175ddefd196d246b9443336539ed8282c35
josephchilin <joseph@josephchilin.com>
2020-07-08 13:03:39 -0400

    updated dog ratings

--- app/controllers/dog_ratings_controller.rb
+++ app/controllers/dog_ratings_controller.rb
@@ -1,2 +1,18 @@
 class DogRatingsController < ApplicationController
+
+    def index
+        @dog_ratings = DogRating.all 
+        render json: @dog_ratings
+    end
+
+    def create
+        @dog_rating = DogRating.create(rating_params)
+        render json: @dog_rating
+    end
+
+    private
+
+    def rating_params
+        params.permit(:rating)
+    end
 end
